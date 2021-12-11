23f089cd0f4e8bbd72262c107e1c93bc4adda854
Melvin Dionisio <75345253+melvindionisio@users.noreply.github.com>
2021-12-11 09:25:35 +0800

    initial  commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App font-montserrat">
+      <div className="container">
+        <header className="p-3 bg-white shadow-sm ">
+          <nav>
+            <h1 className="font-semibold tracking-wide text-center uppercase text-emerald-500">
+              react-tailwind latest-template
+            </h1>
+          </nav>
+        </header>
+        <main className="px-2 py-2">
+          <div className="px-4 mt-10 text-left ">
+            <h2 className="text-5xl font-bold uppercase font-montserrat">
+              Dionisio
+            </h2>
+            <p className="text-2xl font-thin tracking-widest uppercase font-openSans">
+              melvin
+            </p>
+          </div>
+        </main>
+        <footer></footer>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        merriweather: ["Merriweather", "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        openSans: ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
